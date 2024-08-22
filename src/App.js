c25f1b98f20d89376f88bf753c23655aac983a61
buhleqampi <buhle3857@gmail.com>
2024-08-22 14:26:27 +0200

    react lets start working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button>
+      Contact Us
+    </button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+    <div className="about">
+    </div>
+      <p>Saving habitats<br />one generation at a time.</p>
+    </>
+  );
+}
+
+function ServicesPage(){
+  return(
+    <>
+    <h1>Services we offer</h1>
+    <p>Here is a list of services we offer , and are key to our  clientele's satisfaction.</p>
+    </>
+  )
+}
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Safari Lodge</h1>
+      <MyButton />
+      <AboutPage/>
+      <ServicesPage/>
     </div>
   );
 }
 
-export default App;
